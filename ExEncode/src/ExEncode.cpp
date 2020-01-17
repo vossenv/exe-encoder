7b5fba043d08b709d82fff17a8aeef4f851741ae
jansseneda-adobe <danimaejanssen@gmail.com>
2020-01-16 23:25:57 -0600

    change to cmake project

--- ExEncode/src/ExEncode.cpp
+++ ExEncode/src/ExEncode.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include "../include/exencode/exencode.h"
 
+#ifndef CMAKEMACROSAMPLE
+    #define CMAKEMACROSAMPLE "NO SYSTEM NAME"
+#endif
+
 auto sum(int a, int b) {
     return a + b;
 }
@@ -10,6 +14,10 @@ auto sum(int a, int b) {
 
 int main()
 {
+    
+    std::cout << CMAKEMACROSAMPLE;
+
+    std::cout << "x";
 
     std::cout << "Hello World!\n";
 }
